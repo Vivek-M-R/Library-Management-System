67d51eda4d6bfbce398a117a4a05ed2ce27f0acd
Vivek-M-R <gotoclassfast@gmail.com>
2026-06-03 11:29:10 +0530

    Book.java

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    int id;
+    String title;
+    boolean issued;
+
+    public Book(int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.issued = false;
+    }
+}
